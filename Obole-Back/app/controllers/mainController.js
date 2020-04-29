95d46c4f070be10a7c44b70592992b4da5dfe664
LauraFILLATRE <laurafillatre@hotmail.fr>
2020-04-29 15:14:34 +0200

    mise en place du server et router

---
+++ Obole-Back/app/controllers/mainController.js
@@ -0,0 +1,10 @@
+const mainController = {
+
+    homePage:(req,res)=>{
+        res.json({message:'welcome to the home page'})
+    }
+
+
+};
+
+module.exports = mainController;
--- Obole-Back/index.js
+++ Obole-Back/index.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
+const cors=require('cors');
+
 const express=require('express');
-const app=express();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({entended: true}));
+
+const router = require('./app/router');
+app.use(router);
 
 
-
-
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+});
