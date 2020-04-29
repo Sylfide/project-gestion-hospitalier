80d164a1d0648ceb80fa7087e82199b582b33cb3
LauraFILLATRE <laurafillatre@hotmail.fr>
2020-04-29 15:45:22 +0200

    correction sur index.js ligne 11

--- Obole-Back/index.js
+++ Obole-Back/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.urlencoded({entended: true}));
+app.use(express.urlencoded({extended: true}));
 
 const router = require('./app/router');
 app.use(router);
