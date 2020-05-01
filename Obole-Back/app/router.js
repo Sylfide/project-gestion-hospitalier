de02f277f4996ad499d34bee4669a50c3e8e78b0
LauraFILLATRE <laurafillatre@hotmail.fr>
2020-05-01 16:28:15 +0200

    maj du router par rapport à la branche develop

--- Obole-Back/app/router.js
+++ Obole-Back/app/router.js
@@ -3,15 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 // middlewares
+
 const adminAuthentified = require('./middleware/isAdminAuthenticated');
 const userAuthentified = require('./middleware/isUserAuthenticated');
 
 // require des controllers
 const mainController = require('./controllers/mainController');
 const userController = require('./controllers/userController');
+const roomController=require('./controllers/roomController');
 
 // les routes
 router.get('/', mainController.homePage);
+
+// routes pour les users
+router.post('/connection',mainController.connection);
 
 // routes pour les admins / gestion des users
 router.get('/admin/user/list', adminAuthentified, userController.allUsers);
@@ -21,6 +26,12 @@ router.delete('/admin/user/:id/delete', adminAuthentified, userController.delete
 // routes profil
 router.get('/user/:id', userAuthentified, userController.oneUser);
 router.patch('/user/:id', userAuthentified, userController.updateUser);
+
+//routes pour les rooms 
+router.post('/room/new',adminAuthentified,roomController.addRoom);
+router.post('/room/modify/:id',adminAuthentified,roomController.modifyRoom);
+router.get('/room/list',adminAuthentified,roomController.listRooms);
+router.post('/room/details/:id',adminAuthentified,roomController.seeRoom)
 
 // traitement 404
 router.use( (req, res) => {res.status(404).send('404')});
