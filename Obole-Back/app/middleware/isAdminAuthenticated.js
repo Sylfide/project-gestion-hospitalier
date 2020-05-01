1f59c928d8757b442a8a8846c63c15010a194252
LauraFILLATRE <laurafillatre@hotmail.fr>
2020-05-01 12:29:22 +0200

    travail en cours

---
+++ Obole-Back/app/middleware/isAdminAuthenticated.js
@@ -0,0 +1,34 @@
+const db=require('../db_connection.js')
+ const isAdminAuthenticated=async(req,res,next)=>{
+    if(req.headers.authorization){
+       
+        const userToken=req.headers.authorization.replace("Bearer ","");
+        console.log(userToken);
+       
+        const findUser=await db.query(`SELECT * FROM "user" WHERE token=$1`,[userToken]);
+        console.log(findUser.rows);
+        
+        if(findUser.rows[0]){ 
+            if(findUser.rows[0].role!='ROLE_ADMIN' || 'admin'){
+                res.status(401).json({message:'unauthorized'})
+            }
+            else{
+                req.user=findUser.rows[0]
+                next()
+            }
+            
+        }
+
+        else{
+            res.status(401).json({message:'unauthorized'})
+        }
+    }
+
+    else{
+        res.status(400).json({message:'no token sent'})
+    }
+ }
+    
+
+
+module.exports=isAdminAuthenticated;
--- Obole-Back/app/router.js
+++ Obole-Back/app/router.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+// middlewares
+const adminAuthentified = require('./middleware/isAdminAuthenticated');
+const userAuthentified = require('./middleware/isUserAuthenticated');
+
 // require des controllers
 const mainController = require('./controllers/mainController');
 const userController = require('./controllers/userController');
@@ -10,12 +14,12 @@ const userController = require('./controllers/userController');
 router.get('/', mainController.homePage);
 
 // routes pour les admins / gestion des users
-router.get('/admin/user/list', userController.allUsers);
-router.post('/admin/user/new', userController.newUser);
+router.get('/admin/user/list', adminAuthentified, userController.allUsers);
+router.post('/admin/user/new', adminAuthentified, userController.newUser);
 
 // routes profil
-router.get('/user/:id', userController.oneUser);
-router.patch('/user/:id', userController.updateUser);
+router.get('/user/:id', userAuthentified, userController.oneUser);
+router.patch('/user/:id', userAuthentified, userController.updateUser);
 
 // traitement 404
 router.use( (req, res) => {res.status(404).send('404')});
