490b409e69f3c6d757f75736a432fc90e752db35
reubenC <chourakirgit@yahoo.com>
2020-05-05 15:03:28 +0200

    test

--- Obole-Back/app/controllers/mainController.js
+++ Obole-Back/app/controllers/mainController.js
@@ -11,7 +11,7 @@ const mainController = {
          
          
            
-            const findUser=await dataMapper.connection(req.body.email,req.body.password)
+            const findUser=await dataMapper.connection(req.fields.email,req.fields.password)
             
         
             if(findUser[0]){
