afe348352108dd108c24952fc9b7f054909c431f
vincent Del Jesus <vdeljesus@gmail.com>
2020-05-12 15:23:39 +0200

    Parse date fix

--- Obole-Front/src/utils/subForm.js
+++ Obole-Front/src/utils/subForm.js
@@ -1,19 +1,27 @@
+/* eslint-disable camelcase */
 export default (values) => {
+  const birth_date = values.birthDate ? values.birthDate.format('DD/MM/YYYY') : null;
+  const deceased_date = values.deceasedDate ? values.deceasedDate.format('DD/MM/YYYY') : null;
+  const entry_date = values.entryDate ? values.entryDate.format('DD/MM/YYYY') : null;
+  const burial_permit_date = values.burialPermitDate ? values.burialPermitDate.format('DD/MM/YYYY') : null;
+  const exit_date = values.exitDate ? values.exitDate.format('DD/MM/YYYY') : null;
+  const date = values.date ? values.date.format('DD/MM/YYYY') : null;
+
   const form = {
     deceased: {
       lastname: values.lastname,
       firstname: values.firstname,
-      birth_date: values.birth_date,
-      deceased_date: values.deceased_date,
-      entry_date: values.entry_date,
-      burial_permit_date: values.burial_permit_date,
+      birth_date,
+      deceased_date,
+      entry_date,
+      burial_permit_date,
       provenance: values.provenance,
-      exit_date: values.exit_date,
+      exit_date,
       ritual: values.ritual,
       room: values.room,
     },
     conservation: {
-      date: values.date,
+      date,
       embalmer_id: values.embalmer_id,
     },
     deceased_ref: {
