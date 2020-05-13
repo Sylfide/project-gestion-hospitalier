bcdf79f95a53e6e1e3339c5d69e3430e228756a5
vincent Del Jesus <vdeljesus@gmail.com>
2020-05-13 13:05:14 +0200

    User form fix

--- Obole-Front/src/components/FormUser/index.js
+++ Obole-Front/src/components/FormUser/index.js
@@ -1,8 +1,8 @@
 // ==> Import npm
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { creatUser } from 'src/store/actions';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
 // ==> Components
 import {
@@ -12,7 +12,6 @@ import {
   Select,
   Row,
   Col,
-  message,
 } from 'antd';
 
 // ==> Styles
@@ -21,23 +20,14 @@ import {
 const { Option } = Select;
 
 // ==> CSS in JS
+const Container = styled(Form)`
+  margin: 24px 0;
+`;
 
 // ==> Composant
 const FormUser = () => {
   const dispatch = useDispatch();
-  const topMessage = useSelector((state) => state.infoMessage);
-
-  const clear = () => {
-    dispatch({ type: 'clear' });
-  };
-  const error = () => {
-    message.error(topMessage, 2, clear);
-  };
-  useEffect(() => {
-    if (topMessage !== '') {
-      error();
-    }
-  }, [topMessage]);
+  // const clickCount = useSelector((state) => state.counter);
 
   const [form] = Form.useForm();
   const onReset = () => {
@@ -45,8 +35,9 @@ const FormUser = () => {
   };
 
   return (
-    <Form
-      layout="vertical"
+    <Container
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 12 }}
       size="large"
       form={form}
       initialValues={{
@@ -56,93 +47,91 @@ const FormUser = () => {
         dispatch(creatUser(values));
       }}
     >
-      <Col span={12} offset={6}>
-        <Form.Item
-          name="lastname"
-          label="Nom"
-          rules={[
-            {
-              required: true,
-              message: 'Champ requis',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
 
-        <Form.Item
-          name="firstname"
-          label="Prénom"
-          rules={[
-            {
-              required: true,
-              message: 'Champ requis',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+      <Form.Item
+        name="lastname"
+        label="Nom"
+        rules={[
+          {
+            required: true,
+            message: 'Champ requis',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
-        <Form.Item
-          name="email"
-          label="Mail"
-          rules={[
-            {
-              required: true,
-              message: 'Champ requis',
-            },
-            {
-              type: 'email',
-              message: 'Adresse mail pas valide',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+      <Form.Item
+        name="firstname"
+        label="Prénom"
+        rules={[
+          {
+            required: true,
+            message: 'Champ requis',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
-        <Form.Item
-          name="password"
-          label="Mot de passe"
-          rules={[
-            {
-              required: true,
-              message: 'Champ requis',
-            },
-            {
-              min: 8,
-              message: '8 caractères minimum',
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+      <Form.Item
+        name="email"
+        label="Mail"
+        rules={[
+          {
+            required: true,
+            message: 'Champ requis',
+          },
+          {
+            type: 'email',
+            message: 'Adresse mail pas valide',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
-        <Form.Item
-          name="role"
-          label="Rôle de l'utilisateur"
-        >
-          <Select>
-            <Option value="user">Employé</Option>
-            <Option value="admin">Administrateur</Option>
-          </Select>
-        </Form.Item>
+      <Form.Item
+        name="password"
+        label="Mot de passe"
+        rules={[
+          {
+            required: true,
+            message: 'Champ requis',
+          },
+          {
+            min: 8,
+            message: '8 caractères minimum',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-        <Form.Item>
-          <Row justify="center" gutter={32}>
-            <Col>
-              <Button type="primary" htmlType="submit">
-                Enregistrer
-              </Button>
-            </Col>
-            <Col>
-              <Button htmlType="button" onClick={onReset}>
-                Abandonner
-              </Button>
-            </Col>
-          </Row>
-        </Form.Item>
-      </Col>
-    </Form>
+      <Form.Item
+        name="role"
+        label="Rôle de l'utilisateur"
+      >
+        <Select>
+          <Option value="user">Employé</Option>
+          <Option value="admin">Administrateur</Option>
+        </Select>
+      </Form.Item>
+
+      <Row justify="center" gutter={32}>
+        <Col>
+          <Button type="primary" htmlType="submit">
+            Enregistrer
+          </Button>
+        </Col>
+        <Col>
+          <Button htmlType="button" onClick={onReset}>
+            Abandonner
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+
   );
 };
 
