57c90eef2931d41bab33795b0fd66e73cc095e0a
Sylfide <sylviemorin82@gmail.com>
2020-05-13 16:52:14 +0200

    activity dinamique

--- Obole-Front/src/components/Activity/index.js
+++ Obole-Front/src/components/Activity/index.js
@@ -1,45 +1,78 @@
+/* eslint-disable linebreak-style */
 // == Import npm
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router';
+// import { useHistory } from 'react-router';
+import axios from 'axios';
+import { getRooms } from 'src/store/actions';
 
-import { } from 'src/store/actions';
+// import { } from 'src/store/actions';
 
 // Styles
 import './styles.scss';
 
 // == Import
 
-
 // == Composant
 const Activity = () => {
   const dispatch = useDispatch();
-  const clickCount = useSelector((state) => state.counter);
+  const token = useSelector((state) => state.user.token);
+  const rooms = useSelector((state) => state.rooms);
+
+  useEffect(() => {
+    // console.log('je suis chargé')
+    axios({
+      method: 'get',
+      url: 'http://localhost:3000/room/list',
+      headers: { authorization: `Bearer ${token}` },
+    })
+      .then((res) => {
+        dispatch(getRooms(res.data));
+        console.log('data received', res.data)
+      })
+      .catch((error) => {
+        // TODO: error
+        console.log('error: ', error);
+      });
+  }, [token]);
+
+  // const rooms = [
+  //   {
+  //     id: 0,
+  //     name: 'Chambre tinlinlin',
+  //     capacity: 50,
+  //     free: 5,
+  //   },
+  //   {
+  //     id: 1,
+  //     name: 'Chambre truc',
+  //     capacity: 30,
+  //     free: 10,
+  //   },
+  // ]
+  // const dispatch = useDispatch();
+  // const clickCount = useSelector((state) => state.counter);
+
+  // <Option key={room.id} value={room.name}>{room.name}</Option>
+
+  const roomsList = rooms.map((room) => {
+    return (
+      <div className="room" key={room.id}>
+        <h3>{room.name}</h3>
+        <div className="count">
+          <p>Capacité</p>
+          <p className="nbr">{room.capacity}</p>
+          <span />
+          <p>Libre</p>
+          <p className="nbr">{room.occupation}</p>
+        </div>
+      </div>
+    );
+  })
 
   return (
     <div id="activity">
-      <div className="room">
-        <h3>Chambre Tinlinlin</h3>
-        <div className="count">
-          <p>Capacité</p>
-          <p className="nbr">40</p>
-          <span />
-          <p>Libre</p>
-          <p className="nbr">5</p>
-        </div>
-      </div>
-
-      <div className="room">
-        <h3>Chambre Truc</h3>
-        <div className="count">
-          <p>Capacité</p>
-          <p className="nbr">30</p>
-          <span />
-          <p>Libre</p>
-          <p className="nbr">10</p>
-        </div>
-      </div>
-
+      {roomsList}
     </div>
   );
 };
