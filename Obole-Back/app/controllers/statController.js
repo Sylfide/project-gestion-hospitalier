5a1d876f3a58333219d365150b4104f78229ed48
LauraFILLATRE <laurafillatre@hotmail.fr>
2020-05-14 15:00:54 +0200

    work in progress number 2

--- Obole-Back/app/controllers/statController.js
+++ Obole-Back/app/controllers/statController.js
@@ -24,43 +24,54 @@ const statController = {
 
                 for (day = 1; day <= moment("2020-" + (month+1), "YYYY-MM").daysInMonth(); day = day + 7) {
 
-                    let weekStart = '2020-' + month + '-' + day;
-                    let weekEnd = '2020-' + month + '-' + Number(day + 7);
+                    
+                    weekStart = new Date('2020-' + (month + 1) + '-' + day);
+                    // console.log('weekStart :' + weekStart.getMonth());
+
+                    weekEnd = new Date('2020-' + (month + 1) + '-' + Number(day + 7));
+                    // console.log('weekEnd :' + weekEnd.getDate());
                     let total = 0;
+
 
                     // console.log('start : ', weekStart);
                     // console.log('end : ', weekEnd);
 
                     for (stat of stats) {
                         
+                        if (month == 3 || month == 4) {
+                            console.log('debut : ' + total);
+                        }
                         let formatedEntryDate = stat.entry_date.getFullYear() + '-' + stat.entry_date.getMonth() + '-' + stat.entry_date.getDate();
-
-                        if (stat.deceased_id === 25) {
-                            console.log(formatedEntryDate + ' : ' + weekStart);
+                        let formatedExitDate;
+                        if (stat.exit_date !== null) {
+                            formatedExitDate = stat.exit_date.getFullYear() + '-' + stat.exit_date.getMonth() + '-' + stat.exit_date.getDate();
+                        }
+                       
+                        if (stat.entry_date < weekStart || stat.entry_date <= weekEnd && stat.exit_date >= weekStart && stat.exit_date <= weekEnd) {
+                            total++;
+                        } else if (stat.entry_date >= weekStart && stat.entry_date <= weekEnd && stat.exit_date > weekEnd) {
+                            total++;
+                        } else if (stat.entry_date >= weekStart && stat.entry_date <= weekEnd && stat.exit_date >= weekStart && stat.exit_date <= weekEnd) {
+                            total++;
+                        } else if (stat.entry_date <= weekEnd && stat.exit_date == null) {
+                            total++;
                         }
 
-                        if (stat.entry_date.getMonth() <= month && formatedEntryDate >= weekStart) {
-
-                            if (stat.exit_date == null) {
-                                total++;
-                            } else {
-
-                                let formatedExitDate = stat.exit_date.getFullYear() + '-' + stat.exit_date.getMonth() + '-' + stat.exit_date.getDate();
-                                // console.log(formatedExitDate);
-    // 
-                                if (formatedExitDate < weekStart) {
-                                    continue;
-                                }
-                                
-                                if (formatedExitDate >= weekStart && formatedExitDate <= weekEnd) {
-                                    total++;
-                                }
-
-                                if (formatedExitDate > weekEnd) {
-                                    total++;
-                                }
-                            }
+                        if (month == 3 || month == 4) {
+                            console.log('fin : ' + total);
                         }
+                        // il nous reste (pour le mois d'avril) :  --> il faut que l'entrée soit sup à weekStart mais inf à weekEnd && que sa sortie soit sup à weekStart 
+                        // 3 cas : soit l'entrée avant weekStart et sortie entre weekStart et weekEnd 
+                        //          soit l'entrée entre weekStart et weekEnd et sortie après weekEnd
+                        //          soit l'entrée entre weekStart et weekEnd et sortie entre les 2 aussi
+                        // --> sortie null  : soit l'entrée avant weekEnd (inf ou ég) et sortie null 
+                            // 25 : entrée : 28 avril sortie : 5 mai
+                            // 26 : entrée : 28 avril sortie : 5 mai
+                            // 27 : entrée : 28 avril sortie : 5 mai
+                                // weekStart = 27 avril
+                                // weekEnd = 3 mai
+
+                        
                     }
 
                     obj['week ' + weekNb] = total;
