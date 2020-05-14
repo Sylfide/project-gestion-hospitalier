7d42ee5150d33c5dab5e7e02a8f1661f6386ab08
vincent Del Jesus <vdeljesus@gmail.com>
2020-05-14 11:45:22 +0200

    Sort decesead list

--- Obole-Front/src/components/ListDeceased/index.js
+++ Obole-Front/src/components/ListDeceased/index.js
@@ -4,11 +4,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getDeceased } from 'src/store/actions';
 // import styled from 'styled-components';
 import axios from 'axios';
+import moment from 'moment';
+import Highlighter from 'react-highlight-words';
 
 // ==> Components
 import { Table } from 'antd';
 
 // ==> Styles
+import { SearchOutlined } from '@ant-design/icons';
 
 // ==> Ant Design sub components
 const { Column } = Table;
@@ -45,6 +48,8 @@ const ListDeceased = () => {
       key: deceased.id,
       firstname: deceased.firstname,
       lastname: deceased.lastname,
+      birthDate: deceased.birth_date,
+      entryDate: deceased.entry_date,
     };
     return rObj;
   });
@@ -53,9 +58,34 @@ const ListDeceased = () => {
     <Table
       dataSource={data}
       pagination={{ position: ['bottomCenter'], hideOnSinglePage: true }}
+      showSorterTooltip={false}
+      bordered
     >
-      <Column title="Prénom" dataIndex="firstname" key="firstname" />
-      <Column title="Nom" dataIndex="lastname" key="lastname" />
+      <Column
+        title="Prénom"
+        dataIndex="firstname"
+        key="firstname"
+        sorter={(a, b) => a.firstname.localeCompare(b.firstname)}
+      />
+      <Column
+        title="Nom"
+        dataIndex="lastname"
+        key="lastname"
+        sorter={(a, b) => a.lastname.localeCompare(b.lastname)}
+      />
+      <Column
+        title="Date de naissance"
+        dataIndex="birthDate"
+        key="birthDate"
+        render={(text) => moment(text).format('DD/MM/YYYY')}
+        sorter={(a, b) => moment(a.birthDate).unix() - moment(b.birthDate).unix()}
+      />
+      <Column
+        title="Date d'entrée"
+        dataIndex="entryDate"
+        key="entryDate"
+        render={(text) => moment(text).format('DD/MM/YYYY')}
+      />
 
     </Table>
   );
